ffe5feb6cf41ad36ff10c9aab22cab248251909c
Anson <chow.anson25@gmail.com>
2022-08-07 17:02:31 -0400

    React Full Course P1

---
+++ src/components/Image.js
@@ -0,0 +1,8 @@
+import React from "react"
+import MainImage from "./Laura.png"
+
+export default function Image (){
+    return(
+        <img src={MainImage} class="Image" alt="" />
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="container">
+      <h1>Laura Smith</h1>
+      <h2 class="title">Frontend Developer</h2>
+      <p>laurasmith.website</p>
+      <button class = "email__btn">Email</button>
+      <div class="About__section">
+        
+        <h2>About</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam illum
+          illo exercitationem eos soluta blanditiis repellat ipsam similique
+          incidunt laudantium.
+        </p>
+        <h2>Interests</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam illum
+          illo exercitationem eos soluta blanditiis repellat ipsam similique
+          incidunt laudantium.
+        </p>
+      </div>
+    </div>
+  );
+}
